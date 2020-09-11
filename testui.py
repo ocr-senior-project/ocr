c532d055f591df25e2fe43cc31767d2ed098071c
Nate Koike <nkoike1998@gmail.com>
2020-09-12 02:52:53 +0900

    fixed broken-ness (with very hacky code)

--- testui.py
+++ testui.py
@@ -8,7 +8,7 @@
 # WARNING! All changes made in this file will be lost!
 
 import sys
-from PyQt4 import QtCore, QtGui
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -17,37 +17,37 @@ except AttributeError:
         return s
 
 try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
+    _encoding = QtWidgets.QApplication.UnicodeUTF8
     def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+        return QtWidgets.QApplication.translate(context, text, disambig, _encoding)
 except AttributeError:
     def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+        return QtWidgets.QApplication.translate(context, text, disambig)
 
-class Ui_test(object):
+class Ui_test:
     def setupUi(self, test):
         test.setObjectName(_fromUtf8("test"))
         test.resize(1092, 589)
-        self.horizontalLayout = QtGui.QHBoxLayout(test)
+        self.horizontalLayout = QtWidgets.QHBoxLayout(test)
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        self.label = QtGui.QLabel(test)
+        self.label = QtWidgets.QLabel(test)
         self.label.setObjectName(_fromUtf8("label_2"))
         self.horizontalLayout.addWidget(self.label)
-        self.textBrowser = QtGui.QTextBrowser(test)
+        self.textBrowser = QtWidgets.QTextBrowser(test)
         self.textBrowser.setObjectName(_fromUtf8("textBrowser"))
         self.horizontalLayout.addWidget(self.textBrowser)
-        self.verticalLayout = QtGui.QVBoxLayout()
+        self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
 
-        self.pushButton_2 = QtGui.QPushButton(test)
+        self.pushButton_2 = QtWidgets.QPushButton(test)
         self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
         self.pushButton_2.clicked.connect(self.get_file)
 
         self.verticalLayout.addWidget(self.pushButton_2)
-        self.pushButton_3 = QtGui.QPushButton(test)
+        self.pushButton_3 = QtWidgets.QPushButton(test)
         self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
         self.verticalLayout.addWidget(self.pushButton_3)
-        self.pushButton = QtGui.QPushButton(test)
+        self.pushButton = QtWidgets.QPushButton(test)
         self.pushButton.setObjectName(_fromUtf8("pushButton"))
         self.verticalLayout.addWidget(self.pushButton)
         self.horizontalLayout.addLayout(self.verticalLayout)
@@ -63,12 +63,12 @@ class Ui_test(object):
         self.pushButton.setText(_translate("test", "Editing Mode", None))
 
     def get_file(self):
-        fname = QtGui.QFileDialog.getOpenFileName(test, 'Open file','c:\\',"Image files (*.jpg *.gif)")
-        self.label.setPixmap(QtGui.QPixmap(fname))
+        fname = QtWidgets.QFileDialog.getOpenFileName(test, 'Open file','c:\\',"Image files (*.jpeg *.gif)")
+        self.label.setPixmap(QtGui.QPixmap(fname[0]))
 
 if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
-    test = QtGui.QWidget()
+    app = QtWidgets.QApplication(sys.argv)
+    test = QtWidgets.QWidget()
     ui = Ui_test()
     ui.setupUi(test)
     test.show()
