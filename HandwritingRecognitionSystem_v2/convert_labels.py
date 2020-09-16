ee21d24e8742d2f157ecd044463b248fa8271b1f
rd647 <rdanilek@gmail.com>
2020-09-16 14:46:00 -0400

    convert_labels script now takes in relative path

--- HandwritingRecognitionSystem_v2/convert_labels.py
+++ HandwritingRecognitionSystem_v2/convert_labels.py
@@ -1,15 +1,16 @@
-
-with open('samples/Labels/000033/READ17.000033_01_01_pg_line_1486644351571_780.tru') as f:
-    contents = f.read()
+import argparse
 
 with open('samples/CHAR_LIST') as f:
     chars = f.read()
 
-contents = contents.split(' ')
 chars = chars.split('\n')
 
+def label_to_text(relativepath):
+    """ Converts a list of indices of chars in CHAR_LIST to a readable string """
+    with open(relativepath) as f:
+        contents = f.read()
 
-def label_to_text(contents):
+    contents = contents.split(' ')
     text = ''
     for index in contents:
         if index:
@@ -18,6 +19,7 @@ def label_to_text(contents):
     return text
 
 def text_to_label(text):
+    """ Converts a text string into a string of indices of the chars in CHAR_LIST """
     label = ''
     for letter in text:
         if letter == ' ':
@@ -27,6 +29,12 @@ def text_to_label(text):
                 label += str(ind) + ' '
     return label
 
-text = label_to_text(contents)
-print(text)
-print(text_to_label(text))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Convert text file to label string.')
+    parser.add_argument('relativepath', type=str,
+                        help='the relative path to the text file.')
+    args = parser.parse_args()
+    text = label_to_text(args.relativepath)
+    print(text)
+    print(text_to_label(text))
