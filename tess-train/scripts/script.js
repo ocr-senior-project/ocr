80f9684be888035b1a3fc2115e92bf639c94d25d
Nate Koike <nkoike1998@gmail.com>
2020-09-17 04:44:35 +0900

    got good enough bounding boxes when drawing from bl-tr

--- tess-train/scripts/script.js
+++ tess-train/scripts/script.js
@@ -7,16 +7,22 @@ Date: 16 September 2020
 // there is only one file in the directory. this is basically test code
 const src = "images/780.png";
 
-var cvs = document.getElementById("canvas");
-var ctx = cvs.getContext("2d");
+// the amount of padding on the left and top of the canvas
+const pad = 10;
+
+// the desired outline thickness
+const outline = 2;
+
+// get control of the canvas and the context
+let cvs = document.getElementById("canvas"),
+  ctx = cvs.getContext("2d");
 
 // create image
-var img = new Image();
+let img = new Image();
 img.src = "images/780.png";
 
 // the first x and y coords that are clicked
-var firstX;
-var firstY;
+let firstX, firstY;
 
 // load an image
 function loadImage() {
@@ -40,19 +46,71 @@ function loadImage() {
 
 // log the coords to the console
 function mdFunc(event) {
-  firstX = event.pageX;
-  firstY = event.pageY;
-
-  // create a rectangle
-  ctx.beginPath;
+  firstX = event.pageX - pad;
+  firstY = event.pageY - pad;
 }
 
-// call mdFunc on mouse down
-cvs.addEventListener("mousedown", mdFunc);
+function muFunc(event) {
+  let x = event.pageX;
+  let y = event.pageY;
 
-// log the coordinates to the console on mouse up
-cvs.addEventListener("mouseup", event => {
-  console.log(firstX, firstY, event.pageX, event.pageY);
-});
+  // the first x needs to be on the left
+  if (firstX > x) {
+    let swap = x;
+    x = firstX;
+    firstX = swap;
+  }
 
-loadImage();
+  // the first y needs to be on the bottom
+  if (firstY < y) {
+    let swap = y;
+    y = firstY;
+    firstY = swap;
+  }
+
+  console.log(firstX, firstY, x, y);
+
+  // draw a rectangle
+  ctx.beginPath;
+  // left boundary
+  ctx.rect(
+    firstX - outline,
+    firstY - outline,
+    outline,
+    y - firstY - pad + outline
+  );
+
+  // top boundary
+  ctx.rect(
+    firstX - outline,
+    firstY - outline,
+    x - firstX - pad + outline,
+    outline
+  );
+
+  // right boundary
+  ctx.rect(x - pad, firstY - outline, outline, y - firstY - pad + outline);
+
+  // bottom boundary
+  ctx.rect(
+    firstX - outline,
+    y - pad - outline,
+    x - firstX - pad + outline,
+    outline
+  );
+  ctx.fill();
+  ctx.closePath;
+}
+
+function init() {
+  // load the image
+  loadImage();
+
+  // call mdFunc on mouse down
+  cvs.addEventListener("mousedown", mdFunc);
+
+  // log the coordinates to the console on mouse up
+  cvs.addEventListener("mouseup", muFunc);
+}
+
+init();
