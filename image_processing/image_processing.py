70ec531d7cb44dc90010168b87fc5f065f671a8e
Nathan Koike <nkoike1998@gmail.com>
2020-09-24 14:23:05 -1000

    added more image processing options

--- image_processing/image_processing.py
+++ image_processing/image_processing.py
@@ -11,10 +11,17 @@ import numpy as np
 def read(fname):
     return cv2.imread(fname)
 
+# rescale an image
+def scale(image, percent=100):
+    height = int(image.shape[0] * percent / 100)
+    width = int(image.shape[1] * percent / 100)
+
+    return cv2.resize(image, (width, height))
+
 # display an image, closing the window when the escape key is pressed
 def display(image):
     # resize the output window to be 720p
-    cv2.imshow("image", cv2.resize(image, (1280, 720)))
+    cv2.imshow("image", scale(image, 40))
 
     # loop until the window is closed
     while True:
@@ -29,8 +36,8 @@ def threshold(image):
             cv2.THRESH_BINARY, 9, 11)
 
 # noise removal
-def denoise(image):
-    return cv2.medianBlur(image, 3)
+def denoise(image, ksize):
+    return cv2.medianBlur(image, ksize)
 
 # force an image into grayscale
 def grayscale(image):
@@ -51,22 +58,31 @@ def deskew(image):
     return rotated
 
 # apply a gaussian blur to the image
-def blur(image):
-     return cv2.GaussianBlur(image, (5, 5), 0)
+def blur(image, sigma):
+     return cv2.GaussianBlur(image, (sigma, sigma), 0)
 
+# apply a color mask to the image
 def color_mask(image):
     hsv_color = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
+    # the darkest value allowed by the color mask
     low = np.array([0, 0, 0])
 
-    max = 190
+    # the brightest value for each of RGB allowed by the color mask
+    max = 185
 
+    # the lightest value allowed by the color mask
     high = np.array([max, max, max])
+
+    # apply the color mask to the image
     mask = cv2.inRange(hsv_color, low, high)
 
-    mask = blur(mask)
-
     return mask
+
+# invert the black and white sections
+def invert_bw(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV)[1]
+
 
 # get the first line of text in the image
 def get_first_line(image):
--- image_processing/test.py
+++ image_processing/test.py
@@ -12,19 +12,39 @@ ip.display(img)
 # # make the image grayscale
 # img = ip.grayscale(img)
 # ip.display(img)
-
+#
 # # realign the image
 # img = ip.deskew(img)
 # ip.display(img)
-
+#
 # # apply thresholding
 # img = ip.threshold(img)
 # ip.display(img)
 #
 # # denoise the image
-# img = ip.denoise(img)
+# img = ip.denoise(img, 3)
 # ip.display(img)
 
 # mask the image
 img = ip.color_mask(img)
+ip.display(img)
+
+# invert the black and white sections
+img = ip.invert_bw(img)
+ip.display(img)
+
+# # slightly blur the image
+# img = ip.blur(img, 3)
+# ip.display(img)
+
+# apply thresholding
+# img = ip.threshold(img)
+# ip.display(img)
+
+# denoise the image
+img = ip.denoise(img, 3)
+ip.display(img)
+
+# slightly blur the image
+img = ip.blur(img, 5)
 ip.display(img)
