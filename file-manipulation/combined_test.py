37aa7e32bfae0bd6fb7773435347756e6afa4ffc
Nathan Koike <nkoike1998@gmail.com>
2020-09-25 20:16:50 -1000

    added comments

--- file-manipulation/combined_test.py
+++ file-manipulation/combined_test.py
@@ -2,11 +2,14 @@ from img import image_processing as ip
 from pdf import pdf_processing as pp
 import os
 
+# get access to a file in a sub directory
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, 'pdf/matricula_p9.pdf')
 
+# get all the jpgs from the pdf
 lst = pp.get_jpgs(filename)
 
+# for every image in the list
 for image in lst:
     # load the image and double its scale
     display_img = ip.scale(ip.read(image), 200)
