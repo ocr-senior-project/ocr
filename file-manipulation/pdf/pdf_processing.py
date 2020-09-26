9dc2d524e2e18a7459d0d2082aee8f2f0aa73b50
Nathan Koike <nkoike1998@gmail.com>
2020-09-25 19:48:47 -1000

    renamed some files and fixed a bug in pdf_processing.py

--- file-manipulation/pdf/pdf_processing.py
+++ file-manipulation/pdf/pdf_processing.py
@@ -6,14 +6,15 @@ Desc: a slightly more refined way of ripping jpgs from a pdf
 
 # create jpg files for every jpg found in the pdf
 # return a list of all the file names
-def get_jpgs(
-        file, # the pdf in question
-        enc="ansi", # the file encoding (if known, otherwise default to ansi)
-        start_tag="ÿØÿà", # the first characters of the jpg encoding (probably)
-        end_tag="endstream" # the marker for the end of the embedded jpg file
-    ):
+def get_jpgs(fname, enc="ansi", start_tag="ÿØÿà", end_tag="endstream"):
+    # open the file
+    file = open(fname, "rb")
+
     # read the pdf and decode it into a string
     pdf = file.read().decode(enc)
+
+    # close the file
+    file.close()
 
     # the starting index to search from
     start = 0
@@ -34,17 +35,17 @@ def get_jpgs(
         # double check for an image (make sure the search didnt fail)
         if start_tag in stream:
             # save the filename so we can append it to the list as well
-            fname = "jpg" + str(n_images) + ".jpg"
+            img_name = "jpg" + str(len(imgs)) + ".jpg"
 
             # open a .jpg file for writing bytes and write the image to the file
-            jpg = open(fname, "wb")
+            jpg = open(img_name, "wb")
             jpg.write(stream.encode(enc))
 
             # close the file
             jpg.close()
 
             # append the filename to the list
-            imgs.append(fname)
+            imgs.append(img_name)
 
         # start looking at the end of the last jpg that was found
         start = stream_end
--- file-manipulation/pdf/test.py
+++
@@ -1,7 +0,0 @@
-import pdf_processing as pp
-
-file = open("matricula_p9.pdf")
-
-jpgs = pp.get_jpgs(file)
-
-print(jpgs)
