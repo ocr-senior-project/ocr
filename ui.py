e257bc7612c371efddcc0915d386d8c14c7a5f62
vvincent98 <47486254+vvincent98@users.noreply.github.com>
2020-09-26 17:04:35 -0400

    Works w/ Nate's pdf processing now

--- ui.py
+++ ui.py
@@ -1,11 +1,4 @@
-## -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'test.ui'
-#
-# Created: Thu Sep 10 20:52:52 2020
-#      by: PyQt4 UI code generator 4.10.1
-#
-# WARNING! All changes made in this file will be lost!
+# -*- coding: utf-8 -*-
 
 import sys
 from PIL import Image
@@ -40,7 +33,7 @@ class Ui_test:
         test.resize(1092, 589)
         self.horizontalLayout = QtWidgets.QHBoxLayout(test)
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        self.label = QtWidgets.QLabel(test)
+        self.label = ImageLabel()
         self.label.setObjectName(_fromUtf8("label_2"))
         self.horizontalLayout.addWidget(self.label)
 
@@ -48,10 +41,6 @@ class Ui_test:
         self.textBrowser.setObjectName(_fromUtf8("textBrowser"))
         self.horizontalLayout.addWidget(self.textBrowser)
         self.textLayout = QtWidgets.QHBoxLayout(self.textBrowser)
-        #self.child_label = QtWidgets.QLabel(self.textBrowser)
-        #self.child_label_2 = QtWidgets.QLabel(self.textBrowser)
-        #self.textLayout.addWidget(self.child_label)
-        #self.textLayout.addWidget(self.child_label_2)
 
         self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
@@ -88,8 +77,6 @@ class Ui_test:
     def retranslateUi(self, test):
         test.setWindowTitle(_translate("test", "test", None))
         self.label.setText(_translate("test", "                                               PDF Viewer                                                   ", None))
-        #self.child_label.setText(_translate("test","y",None))
-        #self.child_label_2.setText(_translate("test","uh",None))
         self.pushButton_2.setText(_translate("test", "Import PDF", None))
         self.pushButton_3.setText(_translate("test", "Export PDF", None))
         self.pushButton.setText(_translate("test", "Editing Mode", None))
@@ -98,10 +85,10 @@ class Ui_test:
 
     def get_file(self):
         fname = QtWidgets.QFileDialog.getOpenFileName(test, 'Open file','c:\\\\',"Image files (*.jpg *.pdf)")
-        imgs = pp.get_jpgs(fname[0])
         self.page = 0
-        resized = resize_image(imgs[self.page])
-        self.label.setPixmap(QtGui.QPixmap(imgs[self.page]))
+        self.imgs = pp.get_jpgs(fname[self.page])
+        resized = resize_image(self.imgs[self.page])
+        self.label.setPixmap(QtGui.QPixmap(self.imgs[self.page]))
 
     def select(self,label):
         label.setFlat(False)
@@ -109,56 +96,74 @@ class Ui_test:
 
     def show_text(self):
         string = "This is example text! asdfasdfasdf"
-        #characters = []
-        #self.labels = []
         for i in range(0,len(string)):
             new_label = QtWidgets.QPushButton(test)
             self.labels.append(new_label)
-            #characters.append(new_label)
             new_label.setFlat(True)
             new_label.setText(_translate("test",string[i],None))
             self.textLayout.addWidget(new_label)
             new_label.clicked.connect(lambda: self.select(new_label))
 
     def next_page(self):
-        if self.page < len(self.images) - 1:
+        if self.page < len(self.imgs) - 1:
             self.page += 1
-            self.images[self.page].save('out.jpg','JPEG')
-            resized = resize_image('out.jpg')
-            self.label.setPixmap(QtGui.QPixmap('out.jpg'))
+            resized = resize_image(self.imgs[self.page])
+            self.label.setPixmap(QtGui.QPixmap(self.imgs[self.page]))
 
     def previous_page(self):
         if self.page > 0:
             self.page -= 1
-            self.images[self.page].save('out.jpg','JPEG')
-            resized = resize_image('out.jpg')
-            self.label.setPixmap(QtGui.QPixmap('out.jpg'))
+            resized = resize_image(self.imgs[self.page])
+            self.label.setPixmap(QtGui.QPixmap(self.imgs[self.page]))
 
-class MyLabel(QtWidgets.QWidget):
+
+class ImageLabel(QtWidgets.QLabel):
     def __init__(self):
-        super(MyLabel, self).__init__()
-        self.ui = Ui_test()
-        self.ui.setupUi(self)
+        """ Provides event support for the image label """
+        super(ImageLabel, self).__init__()
         self.rubberBand = 0
+        self.selectPolygon = False
+        self.polygonPoints = []
         # self.setMouseTracking(True)
 
     def mousePressEvent(self, event):
+        """ Collects points for the polygon and creates selection boxes """
+        if self.selectPolygon:
+            self.polygonPoints.append((event.x(),event.y()))
+
         self.origin = event.pos()
         if not self.rubberBand:
-            self.rubberBand = QRubberBand(QRubberBand.Rectangle, self)
-        self.rubberBand.setGeometry(QRect(self.origin, QSize()))
+            self.rubberBand = QtWidgets.QRubberBand(QtWidgets.QRubberBand.Rectangle, self)
+        self.rubberBand.setGeometry(QtCore.QRect(self.origin, QtCore.QSize()))
         self.rubberBand.show()
 
     def mouseMoveEvent(self, event):
-        self.rubberBand.setGeometry(QRect(self.origin, event.pos()).normalized())
+        """ Displays the selection box """
+        self.rubberBand.setGeometry(QtCore.QRect(self.origin, event.pos()).normalized())
 
     def mouseReleaseEvent(self, event):
+        """ Hides the selection box """
         self.rubberBand.hide()
         print(self.origin, event.pos())
 
 
+# https://doc.qt.io/qtforpython/PySide2/QtWidgets/QRubberBand.html
+class MainWidget(QtWidgets.QWidget):
+    def __init__(self):
+        """ Calls the UI immediately and provides event support """
+        super(MainWidget, self).__init__()
+        self.ui = Ui_test()
+        self.ui.setupUi(self)
+
+    def keyPressEvent(self, event):
+        """ Called when a key is pressed """
+        if event.key() == QtCore.Qt.Key_Return:
+            self.ui.label.selectPolygon = not self.ui.label.selectPolygon
+            print(self.ui.label.polygonPoints)
+
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    test = MyLabel()
+    test = MainWidget()
     test.show()
     sys.exit(app.exec_())
