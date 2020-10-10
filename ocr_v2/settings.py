e9652d1fe9fd31fae497da0e7af460fa6a03c524
Nate Koike <nkoike1998@gmail.com>
2020-10-10 06:08:39 -1000

    added some methods (still needs updating)

--- ocr_v2/settings.py
+++ ocr_v2/settings.py
@@ -55,10 +55,10 @@ class cfg():
 
         # Name of the model checkpoints
         self.model_name = 'MATRICULAmodel.ckpt'
-        
+
         # Log file
         self.log_file = './log'
-        
+
         # Directory to store Tensorflow summary information
         self.log_dir = './summary'
 
@@ -72,37 +72,68 @@ class cfg():
         # -------- RNN parameters --------
         # Number of LSTM units per forward/backward layer
         self.NUnits = 256
-        
+
         # Number of BLSTM layers
         self.NLayers = 3
 
         # -------- training parameters --------
         # The epoch number to start training from
         self.StartingEpoch = 0 # = 0 to train from scratch, != 0 to resume from the latest checkpoint
-        
+
         # Learning rate
         self.LearningRate = 0.0005
-        
+
         # Batch size
         self.BatchSize = 2 #This is actually the number of images to process each iteration
-        
+
         # Randomize the order of batches each epoch
         self.RandomBatches = True
-        
+
         # Maximum gradient norm
         self.MaxGradientNorm = 5
-        
+
         # Save model each n epochs
         self.SaveEachNEpochs = 1
-        
+
         # Run the training for n epochs
         self.NEpochs = 1000
-        
+
         # Stop the training after n epochs with no improvement on validation
         self.TrainThreshold = 20
 
+    def load_list(fpath, enc="utf-8"):
+        '''
+        load the charset into a string, using whitespace as a delineator
+        return a list of characters and the length of the list
+        fpath: the path to the file containing the character set used by the ocr
+          enc: the encoding of the file. this is utf-8 by default
+        '''
+        # open the file as bytes
+        file = open(fpath, "rb")
 
+        # get the charset by reading in the entire file, decoding it using the
+        # specified encoding, then splitting it on whitespace (default parameter)
+        charset = file.read().decode(enc).split()
 
+        # close the file to avoid file handlers floating around
+        file.close()
 
+        return [charset, len(charset)]
 
+    #Ref: https://stackoverflow.com/questions/33949786/how-could-i-use-batch-normalization-in-tensorflow
+    def batch_norm_conv(x, n_out, phase_train):
+        with tf.compat.v1.variable_scope('bn'):
+            beta = tf.Variable(tf.constant(0.0, shape=[n_out]), name='beta', trainable=True)
+            gamma = tf.Variable(tf.constant(1.0, shape=[n_out]), name='gamma', trainable=True)
+            batch_mean, batch_var = tf.nn.moments(x=x, axes=[0,1,2], name='moments')
+            ema = tf.train.ExponentialMovingAverage(decay=0.5)
 
+            def mean_var_with_update():
+                ema_apply_op = ema.apply([batch_mean, batch_var])
+                with tf.control_dependencies([ema_apply_op]):
+                    return tf.identity(batch_mean), tf.identity(batch_var)
+
+            mean, var = tf.cond(pred=phase_train, true_fn=mean_var_with_update,
+                                false_fn=lambda: (ema.average(batch_mean), ema.average(batch_var)))
+            normed = tf.nn.batch_normalization(x, mean, var, beta, gamma, 1e-3)
+        return normed
